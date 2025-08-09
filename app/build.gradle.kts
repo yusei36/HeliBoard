4330317eeae195c2779e1993b93573b4fcf7cde9
Yusei36 <15942327+yusei36@users.noreply.github.com>
2025-08-09 13:53:32 +0200

    rebrand debug to sdk34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "helium314.keyboard.sdk34"
         minSdk = 21
         targetSdk = 34
-        versionCode = 3002
+        versionCode = 3201
         versionName = "3.2-sdk34"
         ndk {
             abiFilters.clear()
