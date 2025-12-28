c1b5f48de284b9f0a9e2f2790f95fa738568fb86
Yusei36 <15942327+yusei36@users.noreply.github.com>
2025-12-28 16:52:11 +0100

    revert compose version changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -128,12 +128,9 @@ dependencies {
     // compose
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
     implementation(platform("androidx.compose:compose-bom:2025.02.00"))
-    
-    implementation("androidx.compose.ui:ui:1.5.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
-
     implementation("androidx.navigation:navigation-compose:2.8.8")
     implementation("sh.calvin.reorderable:reorderable:2.4.3") // for easier re-ordering
     implementation("com.github.skydoves:colorpicker-compose:1.1.2") // for user-defined colors
