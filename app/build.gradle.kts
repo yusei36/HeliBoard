19bd87f4082d6dbcdd2f11ee590bc3961397e986
Yusei36 <15942327+yusei36@users.noreply.github.com>
2025-12-28 16:27:13 +0100

    try fix build by setting compose version to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -115,6 +115,8 @@ android {
     }
 }
 
+val composeVersion = "1.5.0"
+
 dependencies {
     // androidx
     implementation("androidx.core:core-ktx:1.13.1")
@@ -128,9 +130,12 @@ dependencies {
     // compose
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
     implementation(platform("androidx.compose:compose-bom:2025.02.00"))
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.material3:material3:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    
     implementation("androidx.navigation:navigation-compose:2.8.8")
     implementation("sh.calvin.reorderable:reorderable:2.4.3") // for easier re-ordering
     implementation("com.github.skydoves:colorpicker-compose:1.1.2") // for user-defined colors
